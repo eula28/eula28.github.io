98b28c596871423623e570eb796d7a6a04160248
Genesis Gregorio <genesisgregorio282001@gmail.com>
2024-05-06 15:56:34 +0800

    Content Update for Old Municipal Building

---
+++ script/landmarks_content_functions.js
@@ -0,0 +1,33 @@
+//=======================================================================================================
+// OLD MUNICIPAL BUILDING CONTENT FUNCTIONS
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+// Start slideshow
+showSlides(slideIndex);
+
+function showPhotos() {
+    // Hide the button after it's clicked
+    document.getElementById("showPhotosButton").style.display = "none";
+}
+//=======================================================================================================
+
